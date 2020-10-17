8ff1e2a2887b88378f692dd8995a5983894ca150
Anik Datta <anikdatta05@gmail.com>
2020-10-18 04:13:04 +0530

    Calculating Score and Showing Result
    Show result whether pass or fail out of 5 questions about traffic rules

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,11 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     if (_questionIndex < _questions.length) {
       print("we have more questions");
     } else {
@@ -30,22 +33,59 @@ class _MyAppState extends State<MyApp> {
     });
 
     print(_questionIndex);
-  }
+  } //_answerQuestion
 
   final _questions = const [
     {
-      'questionText': 'What\'s your favourite color?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'questionText': 'What shape is a mandatory traffic sign?',
+      'answers': [
+        {'text': 'Tringle', 'score': 0},
+        {'text': 'Circular', 'score': 1},
+        {'text': 'Rectangular', 'score': 0},
+        {'text': 'All of the above', 'score': 0}
+      ],
     },
     {
-      'questionText': 'What\'s your favourite animal?',
-      'answers': ['Lion', 'Snake', 'ELephant', 'Monkey'],
+      'questionText':
+          "If you're involved in a motor accident, how long do you have to report it to the nearest police station?",
+      'answers': [
+        {'text': '12 hours', 'score': 0},
+        {'text': '48 hours', 'score': 0},
+        {'text': '24 hours', 'score': 1},
+        {'text': '72 hours', 'score': 0}
+      ],
     },
     {
-      'questionText': "What's your favorite sports ?",
-      'answers': ['Cricket', 'Football', 'Basketball', 'Badminton'],
+      'questionText':
+          "Your insurance has expired. How long can you legally drive your vehicle before you renew it?",
+      'answers': [
+        {'text': '0 days', 'score': 1},
+        {'text': '1 week', 'score': 0},
+        {'text': '1 month', 'score': 0},
+        {'text': '3 days', 'score': 0}
+      ],
     },
-  ];
+    {
+      'questionText':
+          "A PUCC (Pollution Under Control Certificate) is valid for how long from the date of issue?",
+      'answers': [
+        {'text': '12 months', 'score': 1},
+        {'text': '6 months', 'score': 0},
+        {'text': '24 months', 'score': 0},
+        {'text': '9 months', 'score': 0}
+      ],
+    },
+    {
+      'questionText':
+          "Once you have obtained your learner's licence, it is valid for how long?",
+      'answers': [
+        {'text': '1 month', 'score': 0},
+        {'text': '1 year', 'score': 0},
+        {'text': '3 months', 'score': 0},
+        {'text': '6 months', 'score': 1}
+      ],
+    },
+  ]; // _questions
 
   @override
   Widget build(BuildContext context) {
@@ -59,7 +99,7 @@ class _MyAppState extends State<MyApp> {
                 answerQuestion: _answerQuestion, //True
                 questionIndex: _questionIndex,
                 questions: _questions)
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    var resultText = 'Completed';
+    if (resultScore >= 3) {
+      resultText = 'You Passed';
+    } else {
+      resultText = 'Failed';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Completed'), //false
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ), //false
     );
   }
 }
